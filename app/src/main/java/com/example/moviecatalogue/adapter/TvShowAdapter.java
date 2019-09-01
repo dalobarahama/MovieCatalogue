5f24cccd15ba1871b108176367f944f03050bea9
dalobarahama <das.barahama@gmail.com>
2019-09-01 21:59:14 +0800

    Migrate to AndroidX

--- app/src/main/java/com/example/moviecatalogue/adapter/TvShowAdapter.java
+++ app/src/main/java/com/example/moviecatalogue/adapter/TvShowAdapter.java
@@ -1,8 +1,8 @@
 package com.example.moviecatalogue.adapter;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +14,6 @@ import com.example.moviecatalogue.R;
 import com.example.moviecatalogue.model.TvShow;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class TvShowAdapter extends RecyclerView.Adapter<TvShowAdapter.TvShowViewHolder> {
 
